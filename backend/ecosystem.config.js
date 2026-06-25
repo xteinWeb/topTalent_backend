6e224b1eb28076902a312d756f67d3dcfac50f2a
joelalbino19 <jj131204@gmail.com>
2026-06-25 11:16:49 -0500

    cambios

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  apps: [
+    {
+      name: 'totem-backend',
+      script: 'index.js',
+      // En producción no se suele activar watch por defecto, pero se puede configurar
+      watch: false,
+
+      // Variables de entorno POR DEFECTO (Producción)
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+        DB_USER: 'sa',
+        DB_PASSWORD: 'sql2025DEVadmin',
+        DB_SERVER: '190.85.54.78',
+        DB_NAME: 'TOP_TALENT',
+        DB_PORT: 9788,
+        DB_ENCRYPT: 'false',
+        FRONTEND_URL: '',
+      },
+
+      // Variables de entorno para Desarrollo (--env development)
+      env_development: {
+        NODE_ENV: 'development',
+        PORT: 3000,
+        DB_USER: 'sa',
+        DB_PASSWORD: 'ADMadm1234',
+        DB_SERVER: '190.85.54.78',
+        DB_NAME: 'TOP_TALENT',
+        DB_PORT: 1433,
+        DB_ENCRYPT: 'false',
+        FRONTEND_URL: 'http://localhost:4200',
+        watch: true // Activa watch en desarrollo para auto-recarga
+      }
+    }
+  ]
+};
